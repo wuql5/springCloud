f0bd95e4e40b86d739589cf193ace1bc482b2684
os.wuql <os.wuql@ffcs.com>
2021-01-28 21:14:37 +0800

    nacos

---
+++ nacos/src/main/java/com/cn/wuql/testLinkNacos.java
@@ -0,0 +1,49 @@
+package com.cn.wuql;
+
+import com.alibaba.nacos.api.NacosFactory;
+import com.alibaba.nacos.api.PropertyKeyConst;
+import com.alibaba.nacos.api.config.ConfigService;
+import com.alibaba.nacos.api.config.listener.Listener;
+import com.alibaba.nacos.api.exception.NacosException;
+
+import java.util.Properties;
+import java.util.concurrent.Executor;
+
+public class testLinkNacos {
+
+
+    public static void main(String[] args) throws NacosException, InterruptedException {
+        String serverAddr = "localhost";
+        String dataId = "workflow-service.yml";
+        String group = "DEFAULT_GROUP";
+        Properties properties = new Properties();
+        properties.put(PropertyKeyConst.SERVER_ADDR, serverAddr);
+        ConfigService configService = NacosFactory.createConfigService(properties);
+        String content = configService.getConfig(dataId, group, 5000);
+        System.out.println(content);
+        configService.addListener(dataId, group, new Listener() {
+            @Override
+            public void receiveConfigInfo(String configInfo) {
+                System.out.println("recieve:" + configInfo);
+            }
+
+            @Override
+            public Executor getExecutor() {
+                return null;
+            }
+        });
+
+        boolean isPublishOk = configService.publishConfig(dataId, group, "content");
+        System.out.println(isPublishOk);
+
+        Thread.sleep(3000);
+        content = configService.getConfig(dataId, group, 5000);
+        System.out.println(content);
+
+        content = configService.getConfig(dataId, group, 5000);
+        System.out.println(content);
+        Thread.sleep(300000);
+
+    }
+
+}
